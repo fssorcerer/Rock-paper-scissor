09b94322992b6424c29c80a3d66e9da62e028747
fssorcerer <[arindamgblog@gmail.com]>
2024-07-29 23:46:43 +0530

    Added function to play five rounds and declare the winner

--- rps.js
+++ rps.js
@@ -66,7 +66,31 @@ function playRound(humanChoice, computerChoice) {
 
   }
   
+  playRound(humanChoice, computerChoice);
+ 
+function playGame() {
+  roundCount = 1
+  while(roundCount <= 4){
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerchoice();
+    playRound(humanSelection,computerSelection);
+    roundCount++;
+  }
+  console.log(humanScore);
+  console.log (computerScore);
+  if(humanScore>computerScore){
+    winner = console.log('Human wins the game');
+  }
+   else if (computerScore > humanScore){
+    winner = console.log('Computer wins the game');
+  }
+  else {
+    winner = console.log('Draw!!');
+  };
+  return winner;
+}
 
+playGame();
   
 
 
